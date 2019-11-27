3f9f66329ea9b31a0be827faec24930a914db281
Petri Asikainen <petri.asikainen@gmail.com>
2019-11-27 17:18:38 +0200

    Tiedostorakenteen uudelleenjärjestelyä

---
+++ api/login/functions/generateUsertoken.php
@@ -0,0 +1,11 @@
+<?php
+    include_once '../../modules/database/Db.php';
+    include_once '../../modules/models/Credential.php';
+
+    function generateUserToken($ownerData)
+    {
+        $salt = "qwerty";
+
+
+    }
+?>
