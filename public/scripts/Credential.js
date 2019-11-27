9078cd245eb0cdc56cd3150fe0d32a1bb80b016b
Tommi Hyvärinen <Tommi.Hyvarinen@edu.savonia.fi>
2019-11-27 17:12:08 +0200

    ui:n aloits

---
+++ public/scripts/Credential.js
@@ -0,0 +1,52 @@
+class Credential {
+    id;
+    ownerId;
+    credentialDescription;
+    username;
+    password;
+    iv;
+    url;
+    
+    constructor() {
+        
+    }
+
+    set(id, ownerId, credentialDescription, username, password, iv, url) {
+        this.id = id;
+        this.ownerId = ownerId;
+        this.credentialDescription = credentialDescription;
+        this.username = username;
+        this.password = password;
+        this.iv = iv;
+        this.url = url;
+    }
+
+    setFromData(data) {
+        this.id = data.id;
+        this.ownerId = data.ownerId;
+        this.credentialDescription = data.credentialDescription;
+        this.username = data.username;
+        this.password = data.password;
+        this.iv = data.iv;
+        this.url = data.url;
+    }
+
+    static fetchAll(usertoken, callback) {
+        let reqData = JSON.stringify({
+            requestType:"read", 
+            usertoken: usertoken
+        });
+
+        $.ajax('api/credentials/', {
+            async: 'false',
+            type: 'POST',
+            data: reqData,
+            success: callback,
+            error: (jqXhr, textStatus, errorMessage) => {
+                console.log(textStatus);
+                console.log(errorMessage);
+                return null;
+            }
+        });
+    }
+}
---
+++ public/scripts/script.js
@@ -0,0 +1,28 @@
+$(() => {
+    const usertoken = "9a8b6d59d6ba5ad7d0b6572603faa3f331225bfa6a069e666a908c85f604e52a2736d3caf217507ee0804d2a03d14d790ff968fc1cd15992fe28ea2fd129c549";
+    Credential.fetchAll(usertoken, (data) => {
+        let returnArray = [];
+        data.data.forEach(cred => {
+            let credential = new Credential();
+            credential.setFromData(cred)
+            returnArray.push(credential);
+        });
+        createCredentialOverview(returnArray);
+    }); 
+});
+
+
+createCredentialOverview = (data) => {
+    let element =  $('<div>').addClass('overview');
+    for (let i = 0; i < data.length; i++) {
+        element.append(createCredentialOverviewElement(data[i]));
+    }
+    $('#appContent').append(element);
+}
+
+createCredentialOverviewElement = (cred) => {
+    let element =  $('<div>').addClass('overviewElement');
+    element.append($('<h4>').text(cred.credentialDescription));
+    element.append($('<p>').addClass('usernameLabel').text('-[ ' + cred.username + ' ]-'));
+    return element;
+}
